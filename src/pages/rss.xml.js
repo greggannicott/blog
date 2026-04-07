4a30ca844ecf8a9bf922853fdc511e794283e58a
Greg Gannicott <greg@gannicott.co.uk>
2026-04-07 07:57:54 +0100

    fix: Exclude feedback files from output

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,23 @@
-import rss, { pagesGlobToRssItems } from "@astrojs/rss";
+import rss from "@astrojs/rss";
+import sanitizeHtml from "sanitize-html";
 
 export async function GET(context) {
+  const postImportResult = import.meta.glob("./**/*.md", { eager: true });
+  const posts = Object.values(postImportResult);
+  const filteredPosts = posts.filter(
+    (post) => !post.file.endsWith("feedback.md"),
+  );
   return rss({
     title: "greg.gannicott.co.uk | Blog",
     description: "Posts from greg.gannicott.co.uk",
     site: context.site,
-    items: await pagesGlobToRssItems(import.meta.glob("./**/*.md")),
+    items: await Promise.all(
+      filteredPosts.map(async (post) => ({
+        link: post.url,
+        content: sanitizeHtml(await post.compiledContent()),
+        ...post.frontmatter,
+      })),
+    ),
     customData: `<language>en-gb</language>`,
   });
 }
