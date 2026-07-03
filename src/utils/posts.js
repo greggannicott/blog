482691bde81520c00df29a5bf72026ee8b4777f8
Greg Gannicott <greg@gannicott.co.uk>
2026-07-03 20:38:20 +0100

    feat: Date ordering

--- src/utils/posts.js
+++ src/utils/posts.js
@@ -1,6 +1,7 @@
 export function getAllPosts() {
   return Object.values(import.meta.glob('../pages/posts/*.md', { eager: true }))
-    .filter(post => !post.file.endsWith("feedback.md"));
+    .filter(post => !post.file.endsWith("feedback.md"))
+    .sort((a, b) => new Date(b.frontmatter.pubDate) - new Date(a.frontmatter.pubDate));
 }
 
 export function getPublishedPosts() {
